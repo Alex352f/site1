bc5716b6cb32bf2f97c9e3fb4255bae2308d738d
Alex352f <57898330+Alex352f@users.noreply.github.com>
2023-12-10 20:18:19 +0500

    Add files via upload

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.addEventListener("DOMContentLoaded", (event) => {
+    const button = document.getElementById("btn");
+    const toggle = document.querySelector('.toggle');
 
-// Write your JavaScript code.
+    button.addEventListener("click", () => {
+        document.body.style.backgroundColor = "#171819";
+        let a = document.createElement('button');
+        a.innerHTML = "X";
+        a.classList.add("toggle");
+        document.querySelector('.upper_footer_text').appendChild(a);
+        toggle.onclick = function () {
+            document.body.style.backgroundColor = "#fff";
+            document.querySelector('.upper_footer_text').removeChild(a);
+        }
+        
+    });
+});
+
+
