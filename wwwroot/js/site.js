5e3a73435dd7a8f68a462d503df4ba301f417af3
Alex352f <57898330+Alex352f@users.noreply.github.com>
2023-12-11 10:37:48 +0500

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,19 +1,16 @@
 ﻿window.addEventListener("DOMContentLoaded", (event) => {
     const button = document.getElementById("btn");
-    const toggle = document.querySelector('.toggle');
+    
 
     button.addEventListener("click", () => {
         document.body.style.backgroundColor = "#171819";
-        let a = document.createElement('button');
-        a.innerHTML = "X";
-        a.classList.add("toggle");
-        document.querySelector('.upper_footer_text').appendChild(a);
-        toggle.onclick = function () {
+        let new_element=document.createElement("button");
+        new_element.innerHTML="X";
+        button.after(new_element);
+
+        new_element.addEventListener("click",()=>{
             document.body.style.backgroundColor = "#fff";
-            document.querySelector('.upper_footer_text').removeChild(a);
-        }
-        
+            new_element.remove();
+        })
     });
 });
-
-
